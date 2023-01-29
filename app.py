b193177b47e35d914c7a5e1ad16a478241a67291
Luis Perez <lperezmo@uoregon.edu>
2023-01-29 02:50:54 -0600

    Updated main application, added a password, added second module and interactive tables to display previously-generated images

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import io
 import urllib.request
 from PIL import Image
 from deta import Deta
+from interactive_table import aggrid_multi_select
 ###########################################################################################################
 # Set page configuration
 st.set_page_config(page_title='Image Generator & Gallery', 
@@ -28,113 +29,158 @@ hide_streamlit_style = """
 			"""
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 ###########################################################################################################
-# Create connection object.
-# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-fs = s3fs.S3FileSystem(anon=False)
-###########################################################################################################
-# Database
-if "deta" not in st.session_state:
-	st.session_state.deta = Deta(st.secrets["DETA_KEY"])
-if "db" not in st.session_state:
-	st.session_state.db = st.session_state.deta.Base("aws-app")
-###########################################################################################################
-# Retrieve file contents.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def read_file(filename):
-    with fs.open(filename) as f:
-        return f.read().decode("utf-8")
+# LOGIN INFO
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["USER_PASS"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
 
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
         return False
-    return True
-
-def create_and_save_image(given_prompt):
-    response = openai.Image.create(
-      prompt=given_prompt,
-      n=1,
-      size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
-    unique_id = response['created']
-
-    # Get current date from pandas
-    now = pd.Timestamp('now')
-    date_string = now.strftime('%Y-%m-%d')
-
-    # Open the image from the URL
-    with urllib.request.urlopen(image_url) as url:
-        s = url.read()
-
-    # Save the image to a file with a given string, current date and time
-    file_name = f"output/{unique_id}_{date_string}.jpg"
-    
-    s3 = boto3.client('s3')
-    with open(file_name, "rb") as f:
-        s3.upload_fileobj(s, "aws-app")
-    
-    # Add entry to database
-    st.session_state.db.put({'id': unique_id, 
-                            'date': date_string, 
-                            'prompt': given_prompt, 
-                            'image': file_name})
-
-    image = Image.open(BytesIO(s))
-
-    return image
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
 ###########################################################################################################
-st.title('Image Generator & Gallery')
-st.write('This app uses OpenAI\'s DALLE API to generate images based on a given prompt. The images are then stored in an AWS S3 bucket and a database.')
-st.write('The images are then displayed in a gallery below.')
+if check_password():
+    # Create connection object.
+    # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+    fs = s3fs.S3FileSystem(anon=False)
+    ###########################################################################################################
+    # Database
+    if "deta" not in st.session_state:
+        st.session_state.deta = Deta(st.secrets["DETA_KEY"])
+    if "db" not in st.session_state:
+        st.session_state.db = st.session_state.deta.Base("aws-app")
+    ###########################################################################################################
+    # Retrieve file contents.
+    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+    @st.experimental_memo(ttl=600)
+    def read_file(filename):
+        with fs.open(filename) as f:
+            return f.read().decode("utf-8")
 
-with st.form(key='create_image'):
-    # Create a text box for the user to enter a prompt
-    prompt = st.text_input('Enter a prompt for the image generator', 'A Shiba Inu wearing a beret and a black turtle neck')
+    def upload_file(file_name, bucket, object_name=None):
+        """Upload a file to an S3 bucket
 
-    # Create a button to generate the image
-    if st.form_submit_button('Generate Image'):
-        image = create_and_save_image(prompt)
-        st.image(image, caption=prompt, use_column_width=True)
+        :param file_name: File to upload
+        :param bucket: Bucket to upload to
+        :param object_name: S3 object name. If not specified then file_name is used
+        :return: True if file was uploaded, else False
+        """
 
-# Display the gallery
-st.write('## Gallery')
-st.write('The gallery below displays all the images generated by the app. Click on an image to view it in full size.')
+        # If S3 object_name was not specified, use file_name
+        if object_name is None:
+            object_name = os.path.basename(file_name)
 
-# Get all the images from the database
-df = st.session_state.db.fetch()
+        # Upload the file
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(file_name, bucket, object_name)
+        except ClientError as e:
+            logging.error(e)
+            return False
+        return True
 
-# Create a list of all the images
-image_list = []
-for image in df:
-    image_list.append(read_file(df['image']))
+    def create_and_save_image(given_prompt):
+        response = openai.Image.create(
+        prompt=given_prompt,
+        n=1,
+        size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        unique_id = response['created']
 
-# Display the images in a gallery
-for idx, img in enumerate(image_list): 
-    cols = st.beta_columns(4) 
+        # Get current date from pandas
+        now = pd.Timestamp('now')
+        date_string = now.strftime('%Y-%m-%d')
+
+        # Open the image from the URL
+        with urllib.request.urlopen(image_url) as url:
+            s = url.read()
+
+        # Save the image to a file with a given string, current date and time
+        file_name = f"{unique_id}_{date_string}.jpg"
+        
+        # Save the image to the local directory
+        with open(file_name, 'wb') as f:
+            f.write(s)
+        
+        upload_file(file_name, "luisappsbucket", object_name=None)
+        
+        # Add entry to database
+        st.session_state.db.put({'id': unique_id, 
+                                'date': date_string, 
+                                'prompt': given_prompt, 
+                                'image': file_name})
+
+        image = Image.open(BytesIO(s))
+
+        return image
+    ###########################################################################################################
+    st.title('Image Generator & Gallery')
+    st.write("This app uses DALL-E's API to generate images based on a given prompt. The images are then stored in an AWS S3 bucket and a database.")
+    st.write('The images are then displayed in a gallery below.')
+
+    with st.form(key='create_image'):
+        # Create a text box for the user to enter a prompt
+        prompt = st.text_input('Enter a prompt for the image generator')
+
+        # Create a button to generate the image
+        if st.form_submit_button('Generate Image'):
+            image = create_and_save_image(prompt)
+            st.image(image, caption=prompt, use_column_width=False, width=500)
+
+    # Display the gallery
+    st.write('## Gallery')
+    st.write('The gallery below displays all the images generated by the app. Click on an image to view it in full size.')
+
+    # Get all the images from the database
+    df = st.session_state.db.fetch().items
+    df = pd.DataFrame(df)
     
-    cols[0].image(image_list[idx], use_column_width=True)
-    idx+=1
-    cols[1].image(image_list[idx], use_column_width=True)
-    idx+=idx
-    cols[2].image(image_list[idx], use_column_width=True)
-    idx+=idx
-    cols[3].image(image_list[idx], use_column_width=True)
-    idx+=idx
+    with st.form("Display selected images"):
+        # Display a list of checkboxes for each image
+        # options = st.multiselect('Select images to display', options = list(df['prompt']))
+        selection = aggrid_multi_select(df)
+
+        # Create a button to display the selected images
+        if st.form_submit_button('Display'):
+            # Display the selected images
+            filtered_selection = selection.iloc[:,1:]
+
+            # Get the selected images
+            selected_df = df[df['prompt'].isin(filtered_selection['prompt'])]
+
+            image_list = []
+
+            cols = st.columns(4)
+            # Display the selected images
+            for index, row in selected_df.iterrows():
+
+                s3 = boto3.client('s3')
+                with open('temp', 'wb') as f:
+                    s3.download_fileobj('luisappsbucket', row['image'], f)
+
+                image = Image.open('temp')
+                image_list.append(image)
+
+                with cols[index%4]:
+                    st.image(image, caption=row['prompt'], use_column_width=True)
+
---
+++ interactive_table.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder, ColumnsAutoSizeMode, JsCode
+from st_aggrid.shared import GridUpdateMode
+import warnings
+from pandas.api.types import (
+	is_categorical_dtype,
+	is_datetime64_any_dtype,
+	is_numeric_dtype,
+	is_object_dtype,
+)
+
+def aggrid_multi_select(df: pd.DataFrame, website = None, list_of_text = None):
+	"""
+	Creates an st-aggrid interactive table based on a dataframe.
+	Return a dataframe with the selected rows.
+	----
+	Parameters:
+	----------
+	df: pd.DataFrame
+		Dataframe to be displayed in the table
+	website: str
+		Website name
+	list_of_text: list
+		List of part numbers to highlight their corresponding rows a light green color.
+		This is used for showing what parts have already been added to the database.
+	
+	Returns:
+	--------
+	df_sel_row: pd.DataFrame
+		Dataframe with the rows selected by the user on interactive table.
+	"""
+	# gd.configure_pagination(enabled=True)
+	gd = GridOptionsBuilder.from_dataframe(df)#, min_column_width=150)
+	gd.configure_side_bar() #Add a sidebar
+	gd.configure_default_column(
+								groupable=True, 
+								# value=True, 
+								enableRowGroup=True,
+								floatingFilter = True,
+								filter = "agSetColumnFilter",
+								# aggFunc="sum",
+								# suppressSyncLayoutWithGrid=True,
+								# contractColumnSelection=True, 
+								# suppressColumnExpandAll = True,
+								# filter=True
+								)
+	for column in df.columns:
+		if is_numeric_dtype(df[column]):
+			gd.configure_column(column, filter = "agNumberColumnFilter")
+		else:
+			gd.configure_column(column, filter = "agTextColumnFilter")
+
+
+	gd.configure_selection(selection_mode="multiple", 
+							# use_checkbox=True, 
+							groupSelectsChildren=True,
+							groupSelectsFiltered=True,
+							rowMultiSelectWithClick=True, 
+							# suppressColumnExpandAll = True,
+							)
+	gridoptions = gd.build()
+	# gridoptions['getRowStyle'] = jscode
+	grid_table = AgGrid(
+						df,
+						# filtered_df.loc[:, cols_to_be_shown],
+						height=600,
+						gridOptions=gridoptions,
+						# fit_columns_on_grid_load=True,
+						update_mode=GridUpdateMode.SELECTION_CHANGED,
+						columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
+						enable_enterprise_modules=True,
+						# allow_unsafe_jscode=True,
+						# theme="material"
+						)
+
+	sel_row = grid_table["selected_rows"]
+	# node_id = grid_table["selected_row_ids"]
+	df_sel_row = pd.DataFrame(sel_row)
+	return df_sel_row
+
+
+def aggrid_single_select(df: pd.DataFrame, website = None):
+	"""
+	Creates an st-aggrid interactive table based on a dataframe.
+	Return a dataframe with the selected rows
+	"""
+	with warnings.catch_warnings():
+		warnings.simplefilter(action='ignore', category=FutureWarning)
+		gd = GridOptionsBuilder.from_dataframe(df)#, min_column_width=150)
+		gd.configure_pagination(enabled=True)
+		gd.configure_side_bar() #Add a sidebar
+		gd.configure_default_column(
+									groupable=True, 
+									# value=True, 
+									enableRowGroup=True,
+									floatingFilter = True,
+									filter = "agSetColumnFilter",
+									# aggFunc="sum",
+									# suppressSyncLayoutWithGrid=True,
+									# contractColumnSelection=True, 
+									# suppressColumnExpandAll = True,
+									# filter=True
+									)
+		for column in df.columns:
+			if is_numeric_dtype(df[column]):
+				gd.configure_column(column, filter = "agNumberColumnFilter")
+			else:
+				gd.configure_column(column, filter = "agTextColumnFilter")
+
+
+		gd.configure_selection(selection_mode="single", 
+								# use_checkbox=True, 
+								groupSelectsChildren=True,
+								groupSelectsFiltered=True,
+								rowMultiSelectWithClick=True, 
+								# suppressColumnExpandAll = True,
+								)
+		gridoptions = gd.build()
+		grid_table = AgGrid(
+							df,
+							# filtered_df.loc[:, cols_to_be_shown],
+							height=600,
+							gridOptions=gridoptions,
+							# fit_columns_on_grid_load=True,
+							update_mode=GridUpdateMode.SELECTION_CHANGED,
+							columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
+							enable_enterprise_modules=True,
+							# theme="material"
+							)
+
+		sel_row = grid_table["selected_rows"]
+		# node_id = grid_table["selected_row_ids"]
+		df_sel_row = pd.DataFrame(sel_row)
+	return df_sel_row
